85c1732cc4f115d5b211830adf2925ab58458301
truongnguyen-msudenver <tnguy189@msudenver.edu>
2017-12-20 16:40:00 -0700

    PSAppDeployToolkit
    Update install and uninstall

--- Deploy-Application.ps1
+++ Deploy-Application.ps1
@@ -57,14 +57,14 @@ Try {
 	##*===============================================
 	## Variables: Application
 	[string]$appVendor = ''
-	[string]$appName = ''
-	[string]$appVersion = ''
-	[string]$appArch = ''
+	[string]$appName = 'Gretl'
+	[string]$appVersion = '2017d'
+	[string]$appArch = 'x64'
 	[string]$appLang = 'EN'
 	[string]$appRevision = '01'
 	[string]$appScriptVersion = '1.0.0'
-	[string]$appScriptDate = '06/12/2017'
-	[string]$appScriptAuthor = '<author name>'
+	[string]$appScriptDate = '12/19/2017'
+	[string]$appScriptAuthor = 'Truong Nguyen'
 	##*===============================================
 	## Variables: Install Titles (Only set here to override defaults set by the toolkit)
 	[string]$installName = ''
@@ -112,7 +112,7 @@ Try {
 		[string]$installPhase = 'Pre-Installation'
 
 		## Show Welcome Message, close applications if required, verify there is enough disk space to complete the install, and persist the prompt
-		Show-InstallationWelcome -CloseApps 'iexplore' -CheckDiskSpace -PersistPrompt
+		Show-InstallationWelcome -CloseApps 'gretl' -CheckDiskSpace -PersistPrompt
 
 		## Show Progress Message (with the default message)
 		Show-InstallationProgress
@@ -132,7 +132,7 @@ Try {
 		}
 
 		## <Perform Installation tasks here>
-
+		Execute-Process -Path "gretl-2017d-64.exe" -Parameters "/verysilent"
 
 		##*===============================================
 		##* POST-INSTALLATION
@@ -152,7 +152,7 @@ Try {
 		[string]$installPhase = 'Pre-Uninstallation'
 
 		## Show Welcome Message, close applications with a 60 second countdown before automatically closing
-		Show-InstallationWelcome -CloseApps 'iexplore' -CloseAppsCountdown 60
+		Show-InstallationWelcome -CloseApps 'gretl' -CloseAppsCountdown 60
 
 		## Show Progress Message (with the default message)
 		Show-InstallationProgress
@@ -172,7 +172,7 @@ Try {
 		}
 
 		# <Perform Uninstallation tasks here>
-
+		Execute-Process -Path "C:\Program Files\gretl\unins000.exe" -Parameters "/verysilent"
 
 		##*===============================================
 		##* POST-UNINSTALLATION
